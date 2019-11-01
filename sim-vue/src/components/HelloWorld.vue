edaf995ffbc385a5dcf7bf6a77c50cc4d3cb1c4e
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-11-01 18:39:53 +0900

    add data

--- sim-vue/src/components/HelloWorld.vue
+++ sim-vue/src/components/HelloWorld.vue
@@ -10,7 +10,7 @@
     <b-container fluid>
 
       <b-row class="my-5">
-        <b-col :cols="6" :sm="2" v-for="(obj, index) in list" :key="index" class="my-2">
+        <b-col :cols="6" :sm="1" v-for="(obj, index) in list" :key="index" class="my-2">
           <router-link v-bind:to="{ name: 'home', query: { u: 'https://raw.githubusercontent.com/nakamura196/cj_sim/master/docs/data/'+obj.id+'.json'}}">
             <b-img-lazy rounded fluid :src="'https://cultural-jp.s3.amazonaws.com/'+obj.id+'.jpg'" :alt="obj.id"></b-img-lazy>
           </router-link>
