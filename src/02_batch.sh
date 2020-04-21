b099a5e178990ec32c525c66747b4425b6198622
Satoru Nakamura <na.kamura.1263@gmail.com>
2020-04-21 22:34:40 +0900

    change dir

--- src/02_batch.sh
+++
@@ -1,6 +0,0 @@
-python 03_process_images.py
-python 04_build.py
-
-# aws s3 sync ../europeana/data/images s3://cultural-jp
-# aws s3 cp ../data s3://cultural-jp/json/ --recursive
-# find arc_nishikie/data/images/ -name "*.jpg" -print0 | xargs -0 -I {} cp {} images/
